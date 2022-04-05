645fa20aea648b7c81f733f8991c1684a36386bc
Valerian Saliou <valerian@valeriansaliou.name>
2022-04-05 22:48:56 +0200

    Page Main Title, to be reused

---
+++ src/components/page/PageMainTitle.vue
@@ -0,0 +1,61 @@
+<!--
+ * This file is part of prose-web
+ *
+ * Copyright 2022, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+  .c-page-main-title
+    base-title(
+      class="c-page-main-title__title"
+    )
+      slot(
+        name="title"
+      )
+
+    base-description(
+      class="c-page-main-title__description"
+    )
+      slot(
+        name="description"
+      )
+
+    .c-page-main-title__action
+      slot(
+        name="action"
+      )
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script>
+export default {
+  name: "PageMainTitle"
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-page-main-title";
+
+.c-page-main-title {
+  text-align: center;
+
+  #{$c}__description {
+    margin-top: 24px;
+  }
+
+  #{$c}__action {
+    margin-top: 40px;
+  }
+}
+</style>
