bc1b329a64ac25a55189db88bf2b2574b12be301
Valerian Saliou <valerian@valeriansaliou.name>
2022-04-09 12:30:53 +0200

    Rack the Crisp chatbox

--- nuxt.config.js
+++ nuxt.config.js
@@ -168,7 +168,7 @@ export default {
   css: ["assets/stylesheets/all"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: "plugins/crisp.js", mode: "client" }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [{ path: "components", pathPrefix: false }],
---
+++ src/plugins/crisp.js
@@ -0,0 +1,25 @@
+/*
+ * This file is part of prose-web
+ *
+ * Copyright 2022, Prose Foundation
+ */
+
+export default function ({ $config }) {
+  // Important: ALWAYS define $crisp, as the Crisp Chatbox SDK might be used \
+  //   here and there in the website code.
+  window.$crisp = [];
+
+  // Insert Crisp? (website identifier is defined)
+  if ($config.tokens.crisp_website_id) {
+    window.CRISP_WEBSITE_ID = $config.tokens.crisp_website_id;
+
+    (function () {
+      const _script = document.createElement("script");
+
+      _script.src = `${$config.url.crisp_client}/l.js`;
+      _script.defer = true;
+
+      document.getElementsByTagName("head")[0].appendChild(_script);
+    })();
+  }
+}
