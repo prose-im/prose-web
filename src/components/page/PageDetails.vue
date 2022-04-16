58c16402f002a919e53a38d51e274671297b9b74
Valerian Saliou <valerian@valeriansaliou.name>
2022-04-16 12:39:38 +0200

    Add page details component

---
+++ src/components/page/PageDetails.vue
@@ -0,0 +1,134 @@
+<!--
+ * This file is part of prose-web
+ *
+ * Copyright 2022, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+  div(
+    :class=`[
+      "c-page-details",
+      "c-page-details--" + align
+    ]`
+  )
+    page-wrapper(
+      class="c-page-details__inner"
+    )
+      .c-page-details__text
+        base-title(
+          level="mini"
+          class="c-page-details__title"
+        )
+          slot(
+            name="title"
+          )
+
+        .c-page-details__description(
+          v-if="$slots.description"
+        )
+          slot(
+            name="description"
+          )
+
+        .c-page-details__actions(
+          v-if="$slots.actions"
+        )
+          slot(
+            name="actions"
+          )
+
+      .c-page-details__illustration(
+        v-if="$slots.illustration"
+      )
+        slot(
+          name="illustration"
+        )
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script>
+export default {
+  name: "PageDetails",
+
+  props: {
+    align: {
+      type: String,
+      default: "left",
+
+      validator(x) {
+        return ["left", "right"].includes(x);
+      }
+    }
+  }
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-page-details";
+
+// VARIABLES
+$illustration-margin-side: 90px;
+
+.c-page-details {
+  #{$c}__inner {
+    display: flex;
+  }
+
+  #{$c}__text,
+  #{$c}__illustration {
+    flex: 1;
+  }
+
+  #{$c}__description {
+    color: $color-black;
+    font-size: 15px;
+    line-height: 21px;
+    margin-top: 26px;
+
+    p {
+      margin-bottom: 18px;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+
+  #{$c}__actions {
+    margin-top: 40px;
+  }
+
+  // --> ALIGNS <--
+
+  &--left {
+    #{$c}__inner {
+      flex-direction: row;
+    }
+
+    #{$c}__illustration {
+      margin-left: $illustration-margin-side;
+    }
+  }
+
+  &--right {
+    #{$c}__inner {
+      flex-direction: row-reverse;
+    }
+
+    #{$c}__illustration {
+      margin-right: $illustration-margin-side;
+    }
+  }
+}
+</style>
