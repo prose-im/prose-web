6f67d42960c1061dcc2827d5d2fb53eedf93b7d0
Valerian Saliou <valerian@valeriansaliou.name>
2022-12-29 20:24:43 +0100

    feat: move away from those unsplash placeholders (to be replaced, still)

--- src/components/section/about/SectionAboutMain.vue
+++ src/components/section/about/SectionAboutMain.vue
@@ -43,7 +43,7 @@ $c: ".c-section-about-main";
 .c-section-about-main {
   #{$c}__illustration-center {
     background-color: $color-base-grey-light;
-    background-image: url("~/assets/images/components/section/about/SectionAboutMain/illustration-placeholder.webp");
+    background-image: url("~/assets/images/components/section/about/SectionAboutMain/illustration-wide.webp");
     background-position: center;
     background-size: cover;
     background-repeat: no-repeat;
