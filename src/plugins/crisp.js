ae76fd7a7fb3cd7f46f9dcb560a6271b8257e542
Valerian Saliou <valerian@valeriansaliou.name>
2023-01-01 13:10:44 +0100

    feat: move to crisp-web-sdk

--- src/plugins/crisp.js
+++ src/plugins/crisp.js
@@ -4,23 +4,24 @@
  * Copyright 2022, Prose Foundation
  */
 
-export default function ({ $config }) {
-  // Important: ALWAYS define $crisp, as the Crisp Chatbox SDK might be used \
-  //   here and there in the website code.
-  window.$crisp = [];
+/**************************************************************************
+ * IMPORTS
+ * ************************************************************************* */
 
+// NPM
+import { Crisp } from "crisp-sdk-web";
+
+/**************************************************************************
+ * EXPORTS
+ * ************************************************************************* */
+
+export default function ({ $config }, inject) {
   // Insert Crisp? (website identifier is defined, and chatbox feature is \
   //   enabled)
   if ($config.tokens.crisp_website_id && $config.features.chatbox === true) {
-    window.CRISP_WEBSITE_ID = $config.tokens.crisp_website_id;
-
-    (function () {
-      const _script = document.createElement("script");
-
-      _script.src = `${$config.url.crisp_client}/l.js`;
-      _script.defer = true;
-
-      document.getElementsByTagName("head")[0].appendChild(_script);
-    })();
+    Crisp.configure($config.tokens.crisp_website_id);
   }
+
+  // Inject $crisp to context
+  inject("crisp", Crisp);
 }
