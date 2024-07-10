f5274439bfaa187088a5fb20643281d3f4a08a0b
Valerian Saliou <valerian@valeriansaliou.name>
2024-07-10 21:17:12 +0200

    feat: add retracted layout

---
+++ src/layouts/retracted.vue
@@ -0,0 +1,23 @@
+<!--
+ * This file is part of prose-web
+ *
+ * Copyright 2024, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+page-container
+  page-header
+
+  page-content
+    slot
+
+  page-entice(
+    :options="false"
+  )
+
+  page-footer
+</template>
