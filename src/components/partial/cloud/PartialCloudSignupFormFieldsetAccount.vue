51b37933d2efb06bb66be9a271d61d649b82fdf1
Valerian Saliou <valerian@valeriansaliou.name>
2024-10-09 19:44:43 +0200

    feat: implement cloud signup account form step

--- src/components/partial/cloud/PartialCloudSignupFormFieldsetAccount.vue
+++ src/components/partial/cloud/PartialCloudSignupFormFieldsetAccount.vue
@@ -10,7 +10,71 @@
 
 <template lang="pug">
 .c-partial-cloud-signup-form-fieldset-account
-  | TODO: form
+  div(
+    :class=`[
+      "c-partial-cloud-signup-form-fieldset-account__part",
+      partClass
+    ]`
+  )
+    base-title(
+      level="lilliput"
+      tint="black"
+    )
+      | What’s your work email?
+
+    form-field(
+      v-model="form.email"
+      name="account_email"
+      type="email"
+      align="left"
+      size="mid-large"
+      placeholder="Enter your work email…  (eg. elon@tesla.com)"
+      autofocus
+    )
+
+  div(
+    :class=`[
+      "c-partial-cloud-signup-form-fieldset-account__part",
+      partClass
+    ]`
+  )
+    base-title(
+      level="lilliput"
+      tint="black"
+    )
+      | Choose a strong password!
+
+    form-field(
+      v-model="form.password"
+      name="account_password"
+      type="password"
+      align="left"
+      size="mid-large"
+      placeholder="Enter a password…"
+    )
+
+    base-security-level(
+      :level="0"
+    )
+      template(
+        v-slot:bad
+      )
+        | Your password is not secure enough.
+
+      template(
+        v-slot:okay
+      )
+        | Your password could be better.
+
+      template(
+        v-slot:good
+      )
+        | Your password looks secure!
+
+      template(
+        v-slot:default
+      )
+        | Please enter a password.
 
   div(
     :class=`[
@@ -50,7 +114,8 @@ export default {
       // --> STATE <--
 
       form: {
-        /* TODO */
+        email: "",
+        password: ""
       }
     };
   },
